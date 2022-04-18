4229bc3f766dec417ed69386653c2ea1b8fc56f9
kikikkiikk <tanqf7@163.com>
2022-04-18 20:23:43 +0800

    添加了修改字数的功能

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@ Also add information on how to contact you by electronic and paper mail.
 */
 #include<iostream>
 #include<fstream>
+#include<cstring>
 using namespace std;
 int main(){
-	int choose;
+	int count;
 	time_t now = time(0);
 	tm *ltm = localtime(&now);
 	int year=1900+ltm->tm_year;
@@ -19,10 +20,10 @@ int main(){
         cout<<"SKFP v0.1 Alpha"<<endl;
         cout<<"SKFP没有任何保证，了解更多请到GitHub页面查看更多."<<endl;
         cout<<"这是一个自由软件，欢迎再次分发本程序，但要满足一定条件."<<endl;
-        cout<<"是否生成检讨后半部分？（1=确认 2=取消）";
-	cin>>choose;
-	if(choose=1){
-	string a[10240],t;
+        cout<<"请输入检讨的字数：";
+	cin>>count;
+	
+	string a[10240],t,out;
 	int i=1,s=0;
 	ifstream readfile("source");
 	ofstream OutFile("jiantao.txt");
@@ -35,27 +36,25 @@ int main(){
 	readfile.seekg(0,ios::beg);
 	int ss=0,r;
 	srand((int)time(0)); 
-	while(ss<1000){
-		r=rand()%s+1;
-		if(a[rand()%s+1]==""){
-			continue;
-		}
-		else{
-			if(a[r]==t){
-				continue;
-			}else{
-				OutFile<<a[r];
-			}
-			t=a[r];
-			ss++;
-		}
-		
-	}
+	while(out.length()<=count){
+    	r=rand()%s+1;
+    	if(a[rand()%s+1]==""){
+    		continue;
+    	}
+    	else{
+    		if(a[r]==t){
+	   			continue;
+	   		}else{
+	   			out+=a[r];
+	   		}
+	    		t=a[r];
+	    		ss++;
+	    	}
+	        cout<<out.length();
+	    }
+    OutFile<<out;
 	readfile.close();
 	cout<<"生成完毕，文件保存在目录下的jiantao.txt。感谢你的使用。"<<endl;
-	system("pause");}
-	else if(choose=2){
-		cout<<"欢迎下次使用"<<endl;
-	}else cout<<"输入有误，请重新打开程序后输入。";
+	system("pause");
 	return 0;
 }
